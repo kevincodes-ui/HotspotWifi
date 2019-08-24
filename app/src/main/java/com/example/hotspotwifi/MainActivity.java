4f3ad7a9bf01b50f6567f2d0ba7826c9a3200c39
kevincodes-ui <kgermain150@gmail.com>
2019-08-24 19:34:07 -0400

    1.10 stage

--- app/src/main/java/com/example/hotspotwifi/MainActivity.java
+++ app/src/main/java/com/example/hotspotwifi/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.hotspotwifi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 
+import java.lang.reflect.Method;
+
 public class MainActivity extends AppCompatActivity {
+    WifiManager wifiManager;
+    WifiConfiguration wifiConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize();
+    }
+
+    private void initialize(){
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiConfiguration = new WifiConfiguration();
+        wifiConfiguration.SSID = "MyDummySSID";
+        changeStateWifiApp(true);
+    }
+
+    private void changeStateWifiApp(boolean activated) {
+        Method method;
+        try {
+            method = wifiManager.getClass().getDeclaredMethod("setWifiApEnabled", WifiConfiguration.class, Boolean.TYPE);
+            method.invoke(wifiManager, wifiConfiguration, activated);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }

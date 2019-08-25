a74494abc183874947c6b820326fe8a099081ae5
kevincodes-ui <kgermain150@gmail.com>
2019-08-24 20:22:49 -0400

    1.11 ui design stage

--- app/src/main/java/com/example/hotspotwifi/MainActivity.java
+++ app/src/main/java/com/example/hotspotwifi/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.hotspotwifi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.lang.reflect.Method;
 
@@ -22,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initialize(){
+        Spinner spinner = findViewById(R.id.security_protocol);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.security_protocol_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         wifiConfiguration = new WifiConfiguration();
         wifiConfiguration.SSID = "MyDummySSID";
@@ -36,5 +51,11 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
